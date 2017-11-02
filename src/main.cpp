45d646f12ada2d2de13b8d159e5e5188196e7c58
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-11-02 17:02:58 -0300

    avances

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
 #include "./mazo.cpp"
 using namespace std;
 
+// Definicion de tipos de cartas
 enum colores {azul, rojo, verde, amarillo, negro};
 enum tipo {mas2=10, reversa=11, salto=12, colores=13, mas4=14};
 
 int main(int argc, char const *argv[]) {
+    // Crear mazo
     Lista_cartas mazo = Lista_cartas();
+
+    // Asignar cartas ordenadas
     for (int color=0; color<5; color++) {
         Carta card;
         int cant_cartas = 0;
+        // Cartas especiales
         if (color == 4){
             for (int i=3; i<5; i++) {
                 card = Carta(color, 10+i);
@@ -23,6 +30,7 @@ int main(int argc, char const *argv[]) {
             }
 
         }
+        // Cartas de colores
         else {
             for (int i=0; i<13; i++) {
                 if (i>=10){
@@ -46,7 +54,36 @@ int main(int argc, char const *argv[]) {
             }
         }
     }
+    // Desordenar mazo
+    //mazo = mazo.randomizar();
 
+    // Crear manos
+    /*Lista_cartas mano = Lista_cartas();
+    for (int i=0; i<7; i++){
+        Carta carta_actual = mazo.sacar();
+        mano.insertar(carta_actual);
+    }*/
+
+    cout << "Entrando \n" << endl;
+    // Enviar mazo a memoria compartida
+     Lista_cartas * shared = (Lista_cartas *)mmap(NULL, sizeof(Carta)*500+sizeof(Lista_cartas), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    //shared = &mazo;
+    //(* shared) = Lista_cartas();
+    cout << "Creado Lista_cartas? \n" << endl;
+    mazo.randomizar(shared); 
+    cout << "Randomizado \n" << endl;
+    shared->primera();
+
+    /*memcpy(shared, &mazo, sizeof(Carta)*101+sizeof(Lista_cartas));
+
+    cout << "Copiando \n" << endl;
+    *shared(mazo);
+    shared->primera();
+    mazo.sacar();
+    mazo.primera();
+    shared->primera();*/
+
+    // Crear hijos
     /*int pid = getpid();
     unsigned short n_jugador;
     for (n_jugador = 4; n_jugador > 1; n_jugador--) {
@@ -59,13 +96,14 @@ int main(int argc, char const *argv[]) {
     cout << "Soy el proceso: " << pid << " y el jugador n: " << n_jugador << "\n";*/
     //printf("Soy el proceso: %d\n", pid);
 
-    mazo.primera();
-    cout << "La primera carta es de color: " << mazo.sacar().getColor() << " y numero: " << mazo.sacar().getNum() << "\n";
+    //mazo.primera();
+    //mano.primera();
+    /*cout << "La primera carta es de color: " << mazo.sacar().getColor() << " y numero: " << mazo.sacar().getNum() << "\n";
     mazo.primera();
     cout << "La segunda carta es de color: " << mazo.sacar().getColor() << " y numero: " << mazo.sacar().getNum() << "\n";
     mazo.primera();
     cout << "La tercera carta es de color: " << mazo.sacar().getColor() << " y numero: " << mazo.sacar().getNum() << "\n";
-    mazo.primera();
+    mazo.primera();*/
 
 
     return 0;
--- src/mazo.cpp
+++ src/mazo.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <list>
+#include <time.h>
 using namespace std;
 
 class Carta {
@@ -20,8 +21,9 @@ public:
 };
 
 class Lista_cartas {
-    list<Carta> cartas;
+
 public:
+	list<Carta> cartas;
     Lista_cartas(){};
     void insertar(Carta card, int posicion = 0) {
         if (posicion == 0) {
@@ -47,10 +49,31 @@ public:
     void primera() {
         int i=0;
         for (auto v : cartas) {
-            cout << "Color: " << v.getColor() << "\n";
-            cout << "Numero: " << v.getNum() << "\n\n";
+            //cout << "Color: " << v.getColor() << "\n";
+            //cout << "Numero: " << v.getNum() << "\n\n";
             i++;
         }
         cout << "Total " << i << "\n";
     };
+    void randomizar(Lista_cartas * mazo) {
+    	srand (time(NULL));
+    	//Lista_cartas aux = Lista_cartas();
+    	int i = 0;
+    	int largo = 108;
+    	do {
+	    	int random_pos = rand() % (i+1);
+	    	Carta carta_actual = this->sacar();
+	    	mazo->insertar(carta_actual, random_pos);
+	    	i++;
+    	} while (i<largo);
+    	//return aux;
+    };
+    /*Lista_cartas copy() {
+    	Carta aux_card;
+    	int largo = this->largo();
+    	int i = 0;
+    	while (i < largo) {
+    		aux_card = this->sacar
+    	}
+    }*/
 };
