9a65d9072d5bce61307926f696c2b2d9801bf5a5
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-11-03 02:14:03 -0300

    seleccion basica de jugador

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,15 @@ int main(int argc, char const *argv[]) {
     Mazo * shared = (Mazo *)mmap(NULL, sizeof(Carta)*108+sizeof(Mazo), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
     (* shared) = mazo;
     
+    // Manejo de pipes
+    char turno[1];
+    int pipeAtoB[2], pipeBtoC[2], pipeCtoD[2], pipeDtoA[2], pipeJugada[2];
+    pipe(pipeAtoB);
+    pipe(pipeBtoC);
+    pipe(pipeCtoD);
+    pipe(pipeDtoA);
+    pipe(pipeJugada);
+
     // Crear hijos
     int pid = getpid();
     unsigned short n_jugador;
@@ -72,7 +81,7 @@ int main(int argc, char const *argv[]) {
     }
 
     cout << "Soy el proceso: " << pid << " y el jugador n: " << n_jugador << "\n";
-    printf("Soy el proceso: %d\n", pid);
+    printf("Soy el proceso: %d\n\n", pid);
 
     // Crear manos
     Mano hand = Mano();
@@ -81,15 +90,51 @@ int main(int argc, char const *argv[]) {
         hand.insertar(carta_actual);
     }
 
-    //hand.primera();
-    if (pid != 0){
-        sleep(3);
-        shared->primera();
+
+    // Condiciones iniciales
+    if (n_jugador == 4) {
+        close(pipeDtoA[0]);
+        write(pipeDtoA[1] ,"1", 1); 
     }
 
+    int estado_turno;
+    cout << "***n_jugador:*** " << n_jugador << "\n";
+    while (true) {
+        sleep(1);
+
+        // Leer del pipe correspondiente
+        if (n_jugador == 1) {
+            close(pipeDtoA[1]);
+            read(pipeDtoA[0], turno, 1);
+
+        }
+        else if (n_jugador == 2) {
+            close(pipeAtoB[1]);
+            read(pipeAtoB[0], turno, 1);
+        }
+        else if (n_jugador == 3) {
+            close(pipeBtoC[1]);
+            read(pipeBtoC[0], turno, 1);
+        }
+        else if (n_jugador == 4){
+            close(pipeCtoD[1]);
+            read(pipeCtoD[0], turno, 1);
+        }
+        else {
+            cout << "Error seleccionando al hijo \n" << endl;
+            break;
+        }
+        
+        estado_turno = atoi(turno);
+
+        cout << "n_jugador: " << n_jugador << "\n";
+        
+        
+    }
     
     
 
+    hand.borrar();
 
     return 0;
 }
