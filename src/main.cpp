d31cfaf9f8296ce73071b3be628cdb6ae54ae4bf
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-11-04 21:32:49 -0300

    avance reversa

--- src/main.cpp
+++ src/main.cpp
@@ -65,12 +65,17 @@ int main(int argc, char const *argv[]) {
     
     // Manejo de pipes
     char turno[1];
-    int pipeAtoB[2], pipeBtoC[2], pipeCtoD[2], pipeDtoA[2], pipeJugada[2];
+    int pipeAtoB[2], pipeBtoC[2], pipeCtoD[2], pipeDtoA[2];
+    int pipeJugadaAtoB[2], pipeJugadaBtoC[2], pipeJugadaCtoD[2], pipeJugadaDtoA[2];
     pipe(pipeAtoB);
     pipe(pipeBtoC);
     pipe(pipeCtoD);
     pipe(pipeDtoA);
-    pipe(pipeJugada);
+    // Pipes de jugada
+    pipe(pipeJugadaAtoB);
+    pipe(pipeJugadaBtoC);
+    pipe(pipeJugadaCtoD);
+    pipe(pipeJugadaDtoA);
 
     // Crear hijos
     int pid = getpid();
@@ -82,8 +87,8 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    cout << "Soy el proceso: " << pid << " y el jugador n: " << n_jugador << "\n";
-    printf("Soy el proceso: %d\n\n", pid);
+    /*cout << "Soy el proceso: " << pid << " y el jugador n: " << n_jugador << "\n";
+    printf("Soy el proceso: %d\n\n", pid);*/
 
     // Crear manos
     Mano hand = Mano();
@@ -93,26 +98,36 @@ int main(int argc, char const *argv[]) {
     }
 
 
-    // Seleccionas pipes
+    // Seleccionas pipes para verificar turno y escribir jugada
     int salir = 0;
     int * pipeRead = (int *)malloc(sizeof(int)*2);
     int * pipeWrite = (int *)malloc(sizeof(int)*2);
+    int * pipeJugadaRead = (int *)malloc(sizeof(int)*2);
+    int * pipeJugadaWrite = (int *)malloc(sizeof(int)*2);
 
     if (n_jugador == 1) {
         pipeRead = pipeDtoA;
         pipeWrite = pipeAtoB;
+        pipeJugadaRead = pipeJugadaDtoA;
+        pipeJugadaWrite = pipeJugadaAtoB;
     }
     else if (n_jugador == 2) {
         pipeRead = pipeAtoB;
         pipeWrite = pipeBtoC;
+        pipeJugadaRead = pipeJugadaAtoB;
+        pipeJugadaWrite = pipeJugadaBtoC;
     }
     else if (n_jugador == 3) {
         pipeRead = pipeBtoC;
         pipeWrite = pipeCtoD;
+        pipeJugadaRead = pipeJugadaBtoC;
+        pipeJugadaWrite = pipeJugadaCtoD;
     }
     else if (n_jugador == 4){
         pipeRead = pipeCtoD;
         pipeWrite = pipeDtoA;
+        pipeJugadaRead = pipeJugadaCtoD;
+        pipeJugadaWrite = pipeJugadaDtoA;
     }
     else {
         cout << "Error seleccionando al hijo \n" << endl;
@@ -127,136 +142,145 @@ int main(int argc, char const *argv[]) {
         Carta inicial = shared->sacar();
         int col = inicial.getColor();
         int num = inicial.getNum();
+        showCards(col, num);
+        cout << "\n" << endl;
+
+        //**********************************/
+        //**********************************//
+        //**********************************//
+        // Testeo
+        num = reversa;
+        col = azul;
+        //**********************************//
+        //**********************************//
+        //**********************************//
 
         // Actualizar ultima jugada
         last_card->setNum(num);
         last_card->setColor(col);
 
+        
+
         // Comunicar jugada codificada
+        // En orden: "Sentido + N_jugadores a saltar + Color cambiado + Más cartas acumuladas"
         string play;
         if (num == reversa) {
-            play = "003";
+            play = "10003";
+            cout << "Revirtiendo..." << endl;
         }
         else if (num == salto) {
-            play = "002";
+            play = "00002";
         }
         else if (num == mas2) {
-            play = "002";
+            play = "00002";
         }
         else if (num == mas4) {
-            play = "904";
+            play = "00904";
         }
         else if (num == colores) {
-            play = "000";
+            play = "00000";
         }
         // Escribir jugada
-        close(pipeJugada[0]);
-        write(pipeJugada[1], play.c_str(), 3);
+        cout << "Escribi play inicial: " << play.c_str() << endl;
+        close(pipeJugadaWrite[0]);
+        write(pipeJugadaWrite[1], play.c_str(), 5);
 
-        // Comunicar turno
+        // Comunicar turno inicial
         close(pipeDtoA[0]);
-        write(pipeDtoA[1] ,"1", 1);
+        write(pipeDtoA[1] ,"2", 1);
     }
 
     int estado_turno;
-    int color_mas4;
+    int color_mas4, sentido, n_reversa;
     int estado_jugada;
     while (!salir) {
-        sleep(1);
+        //sleep(1);
+
         // Leer del pipe correspondiente
         close(pipeRead[1]);
         read(pipeRead[0], turno, 1);
         estado_turno = atoi(turno);
 
+        cout << "Lei mi turno y soy el jugador " << n_jugador << endl;
+        cout << " Lei estado de turno: " <<estado_turno<<endl;
+
         // Si le toca al jugador
-        if (estado_turno == 1) {
+        if (estado_turno == 1 || estado_turno == 2) {
             // Leer última jugada
             int col = last_card->getColor();
             int num = last_card->getNum();
 
             // Verificar estado de última jugada
-            if (col == negro || num == reversa || num == mas2 || num == salto) {
-                // Leer condiciones actuales
-                string play;
-                char play_aux[3];
-                close(pipeJugada[1]);
-                read(pipeJugada[0], play_aux, 3);
-                play = play_aux;
-                color_mas4 = stoi(play.substr(0, 1));
-                estado_jugada = stoi(play.substr(1, 3));
-
-                // Verificar jugada específica
-                if (num == salto) {
-                    estado_jugada--;
-                    if (estado_jugada == 1) {
-                        cout << "Saltando al jugador" << n_jugador <<"\n" << endl;
-                        close(pipeWrite[0]);
-                        write(pipeWrite[1], "001", 3);
-                        continue;
-                    }
-                }
-                else if (num == reversa) {
-                    estado_jugada--;
-                    if (estado_jugada>0) {
-                        play = "00" + to_string(estado_jugada);
-                        close(pipeWrite[0]);
-                        write(pipeWrite[1], "1", 1);
-                    }
-                }
-
+            string play;
+            char play_aux[5];
+            close(pipeJugadaRead[1]);
+            read(pipeJugadaRead[0], play_aux, 5);
+            play = play_aux;
+            // Estado de jugada
+            sentido = stoi(play.substr(0, 1));
+            n_reversa = stoi(play.substr(1, 2));
+            color_mas4 = stoi(play.substr(2, 3));
+            estado_jugada = stoi(play.substr(3, 5));
+            // Avanzar al jugador
+            if (estado_turno != 2 && sentido==1 && n_reversa > 0) {
+                n_reversa--;
+                play = to_string(sentido)+to_string(n_reversa)+to_string(color_mas4)+to_string(estado_jugada);
+                // Pasar jugada
+                close(pipeJugadaWrite[0]);
+                write(pipeJugadaWrite[1], play.c_str(), 5);
+                // Avanzar jugador
+                close(pipeWrite[0]);
+                write(pipeWrite[1], "1", 1);
+                continue;
             }
-            /*if (num == reversa) {
-                close(pipeJugada[1]);
-                read(pipeJugada[0], last_play, 2);
-                estado_jugada = atoi(last_play);
+            // Verificar jugada específica
+            // Corresponde saltar
+            if (num == salto) {
                 estado_jugada--;
-                // Si no me toca, saltar
-                if (estado_jugada) {
-                    if (estado_jugada >= 10) {
-                        *last_play = estado_jugada+'0';
-                    }
-                    else {
-                        last_play[0] = '0';
-                        last_play[1] = estado_jugada+'0';
-                    }
-                    // Actualizar jugador
-                    close(pipeJugada[0]);
-                    write(pipeJugada[1], last_play, 2);
-                    // Informar al siguiente
+                if (estado_jugada == 1) {
+                    cout << "Saltando al jugador " << n_jugador <<"\n" << endl;
+                    // Actualizar jugada
+                    play = to_string(sentido)+"0001";
+                    close(pipeJugadaWrite[0]);
+                    write(pipeJugadaWrite[1], play.c_str(), 5);
+                    // Pasar jugador
                     close(pipeWrite[0]);
                     write(pipeWrite[1], "1", 1);
+                    continue;
                 }
             }
-            else if (num == salto) {
-                close(pipeJugada[1]);
-                read(pipeJugada[0], last_play, 2);
-                estado_jugada = atoi(last_play);
+            // Cambiar sentido
+            else if (num == reversa && estado_turno!=2) {
                 estado_jugada--;
-                if (estado_jugada) {
-                    // Actualizar jugador
-                    close(pipeJugada[0]);
-                    write(pipeJugada[1], "01", 2);
-                    // Informar al siguiente
+                if (estado_jugada>0) {
+                    sentido = (sentido+1)%2;
+                    n_jugador = 4;
+                    play = to_string(sentido)+to_string(n_reversa)+ "00" + to_string(estado_jugada);
+                    // Actualizar jugada
+                    close(pipeJugadaWrite[0]);
+                    write(pipeJugadaWrite[1], play.c_str(), 5);
+                    // Pasar jugador
                     close(pipeWrite[0]);
                     write(pipeWrite[1], "1", 1);
+                    continue;
                 }
             }
-            else if (num == mas2 || num == mas4) {
-                close(pipeJugada[1]);
-                read(pipeJugada[0], last_play, 2);
-                estado_jugada = atoi(last_play);
-            }*/
 
 
 
-            // Imprimir última jugada al jugador actual
-            showCards(col, num);
-            cout << "\n" << endl;
+            
 
 
 
             // Avanzar al jugador actual
-            cout << "*** Turno del jugador número " << n_jugador << " ***\n\n";
+            cout << "****************************************\n";
+            cout << "****** Turno del jugador número " << n_jugador << " ******\n";
+            cout << "****************************************\n\n";
+
+            // Imprimir última jugada al jugador actual
+            cout << "Última carta jugada: \n";
+            showCards(col, num);
+            cout << "\n" << endl;
 
             // Mostrar mano
             cout << "Sus cartas actuales son: \n" << endl;
@@ -313,6 +337,9 @@ int main(int argc, char const *argv[]) {
 
 
                 }
+
+                // Verificar mano
+                
                 // Caso de jugar carta
                 else {
                     // Sacar carta de la mano
@@ -335,6 +362,8 @@ int main(int argc, char const *argv[]) {
     
     free(pipeRead);
     free(pipeWrite);
+    free(pipeJugadaRead);
+    free(pipeJugadaWrite);
     hand.borrar();
     if (pid != 0) {
         munmap(shared, sizeof(Carta)*108+sizeof(Mazo));
